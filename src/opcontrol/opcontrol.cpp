49c47bdb6d2014c5660bcbc7eb8a21420978619b
pltw <pltw@AHS-17-HNNR4F2.acs.edu>
2019-07-24 11:07:49 -0500

    ima live like i aint knew her

--- src/opcontrol/opcontrol.cpp
+++ src/opcontrol/opcontrol.cpp
@@ -1,8 +1,16 @@
 #include "main.h"
-
+pros::ADIEncoder lEncode(1,false);
+pros::ADIEncoder rEncode(3,true);
+void resetPress() {
+	lEncode.reset();
+	rEncode.reset();
+}
 void opcontrol() {
 
 	//Identify which controller is the master constroller
+	pros::lcd::initialize();
+  pros::lcd::register_btn1_cb(resetPress);
+
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 	//Identify all the motors
@@ -10,8 +18,12 @@ void opcontrol() {
 	pros::Motor bRight(2);
 	pros::Motor fLeft(3);
 	pros::Motor bLeft(4);
+	lEncode.reset();
+	rEncode.reset();
 
 	while (true) {
+		pros::lcd::set_text(1,"Left Encoder: " + std::to_string(lEncode.get_value()));
+		pros::lcd::set_text(2,"Right Encoder: " + std::to_string(rEncode.get_value()));
 		int leftY = master.get_analog(ANALOG_LEFT_Y);
 		int leftX = master.get_analog(ANALOG_LEFT_X);
 
